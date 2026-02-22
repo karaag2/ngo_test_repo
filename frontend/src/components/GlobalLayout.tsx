495b34fb432718df745a27e3327d3f96d6f9f04b
karaag2 <amosissa6@gmail.com>
2026-02-22 16:37:09 +0100

    first commit

---
+++ frontend/src/components/GlobalLayout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GlobalLayout = () => {
+  return (
+    <div>
+        <header></header>
+        <main></main>
+        <footer></footer>
+    </div>
+  )
+}
+
+export default GlobalLayout
