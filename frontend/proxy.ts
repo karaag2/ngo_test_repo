b366b616564ad4cd9942232c2981d881e786ef9d
karaag2 <amosissa6@gmail.com>
2026-03-07 23:36:43 +0100

    style: refine proxy redirection logic

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -5,19 +5,24 @@ export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const refreshToken = request.cookies.get("refreshToken");
 
-  const isAuthPath = pathname.startsWith("/admin/auth");
-  const isDashboardPath = pathname.startsWith("/admin/dashboard");
-  const isLoginPath = pathname === "/admin/auth/login";
   const isSignupPath = pathname === "/admin/auth/signup";
-  const isVerify2FAPath = pathname === "/admin/auth/verify-2fa";
   const isSetup2FAPath = pathname === "/admin/auth/setup-2fa";
+  const isVerify2FAPath = pathname === "/admin/auth/verify-2fa";
+  const isLoginPath = pathname === "/admin/auth/login";
+  const isDashboardPath = pathname.startsWith("/admin/dashboard");
+  const isAuthPath = pathname.startsWith("/admin/auth");
 
   if (!refreshToken) {
-    if (pathname.startsWith("/admin") && !isLoginPath && !isVerify2FAPath) {
+    if (
+      pathname === "/admin" ||
+      isDashboardPath ||
+      isSignupPath ||
+      isSetup2FAPath
+    ) {
       return NextResponse.redirect(new URL("/admin/auth/login", request.url));
     }
   } else {
-    if (isLoginPath || isVerify2FAPath) {
+    if (isAuthPath && !isSignupPath && !isSetup2FAPath) {
       return NextResponse.redirect(new URL("/admin/dashboard", request.url));
     }
   }
